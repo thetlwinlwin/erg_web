89e376330ddcefcb34574285dcc07533d52416d5
thetlwinlwin <minthantlwin8@gmail.com>
2022-12-17 08:41:39 +0630

    mobile product page and scrollbar

--- lib/home/home_body.dart
+++ lib/home/home_body.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:erg_web/shared/page_inherited.dart';
+import 'package:erg_web/shared/responsiveness.dart';
 import 'package:flutter/material.dart';
 
 class BodyView extends StatelessWidget {
@@ -7,11 +10,32 @@ class BodyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = HomePageControl.of(context).controller;
-    return PageView(
-      onPageChanged: (value) {},
-      controller: controller,
-      scrollDirection: Axis.vertical,
-      children: PageOrder.pageList,
+    return ScrollConfiguration(
+      behavior: ScrollConfiguration.of(context).copyWith(
+        dragDevices: {
+          PointerDeviceKind.touch,
+          PointerDeviceKind.mouse,
+        },
+      ),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < ScreenSize.phone.maxWidth) {
+            return PageView(
+              controller: controller,
+              scrollDirection: Axis.vertical,
+              children: PageOrder.pageList,
+            );
+          }
+          return Scrollbar(
+            controller: controller,
+            child: PageView(
+              controller: controller,
+              scrollDirection: Axis.vertical,
+              children: PageOrder.pageList,
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/home_pages/products/mobile_products.dart
+++ lib/home_pages/products/mobile_products.dart
@@ -1,3 +1,4 @@
+import 'package:erg_web/shared/product_model.dart';
 import 'package:flutter/material.dart';
 
 class MobileProductsPageView extends StatelessWidget {
@@ -5,8 +6,21 @@ class MobileProductsPageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return GridView.builder(
+      scrollDirection: Axis.horizontal,
+      semanticChildCount: productList.length,
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 300,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+      ),
+      itemBuilder: (context, index) => GridTile(
+        child: Image(
+          image: AssetImage(productList[index].imgPath),
+          fit: BoxFit.scaleDown,
+        ),
+      ),
+      itemCount: productList.length,
     );
   }
 }
